6e462b60b462175ecc10f778f551e860cb114fda
Dariusz Sikorski <dariusz.sikorski@bluesoft.com>
2022-04-25 13:45:53 +0200

    Add handler for ViolationExceptions

---
+++ src/main/java/com/sikorski/commission/configuration/CustomErrorHandler.java
@@ -0,0 +1,21 @@
+package com.sikorski.commission.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import javax.validation.ConstraintViolationException;
+import java.io.IOException;
+
+@Slf4j
+@ControllerAdvice
+public class CustomErrorHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public void handleConstraintViolationException(ConstraintViolationException exception,
+                                                   ServletWebRequest webRequest) throws IOException {
+        webRequest.getResponse().sendError(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    }
+}
