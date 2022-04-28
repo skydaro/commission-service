9f22a659c4e43ec6913a3e4cd13811d4240596d1
Dariusz Sikorski <dariusz.sikorski@bluesoft.com>
2022-04-28 12:19:50 +0200

    Add amount validation to TransactionRequest

--- src/main/java/com/sikorski/commission/api/controller/dto/TransactionRequest.java
+++ src/main/java/com/sikorski/commission/api/controller/dto/TransactionRequest.java
@@ -3,6 +3,7 @@ package com.sikorski.commission.api.controller.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,6 +16,7 @@ public class TransactionRequest {
     LocalDate date;
     @NotNull(message = "Amount can not be null")
     @NotEmpty(message = "Amount can not be empty")
+    @Digits(integer = 18, fraction = 2)
     String amount;
     @NotNull(message = "Currency can not be null")
     @Size(max = 3, min = 3, message = "Currency must be 3 characters")
