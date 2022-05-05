f96c200d9ca2ac8cb24154ed4cbf24317f745434
Dariusz Sikorski <dariusz.sikorski@bluesoft.com>
2022-05-05 16:01:06 +0200

    Add swagger descriptions

--- src/main/java/com/sikorski/commission/api/controller/CommissionController.java
+++ src/main/java/com/sikorski/commission/api/controller/CommissionController.java
@@ -3,6 +3,12 @@ package com.sikorski.commission.api.controller;
 import com.sikorski.commission.api.controller.dto.CommissionResponse;
 import com.sikorski.commission.api.controller.dto.TransactionRequest;
 import com.sikorski.commission.application.CommissionService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +23,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@OpenAPIDefinition(info = @Info(
+        title = "commission-service",
+        description = "REST API for transaction commission calculation",
+        version = "0.0.1"))
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/commission")
@@ -26,6 +36,11 @@ public class CommissionController {
 
     private final CommissionService service;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(hidden = true)))
+    })
     @PostMapping()
     @Synchronized()
     public ResponseEntity<CommissionResponse> getCommission(@Valid @RequestBody TransactionRequest request,
